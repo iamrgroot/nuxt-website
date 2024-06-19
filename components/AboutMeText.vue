56dc1361971529a52d63b12cd51c1756c105019d
Robert Groot <github@rgroot.nl>
2024-06-19 17:56:24 +0000

    fix: about me update

--- components/AboutMeText.vue
+++ components/AboutMeText.vue
@@ -3,9 +3,11 @@
     I'm Robert, a full stack developer at
     <ImageLink href="https://paqt.com" image="/paqt.png" text="PAQT.com" />
     and I am mainly working with
-    <ImageLink href="https://laravel.com" icon="mdi:laravel" text="Laravel" />
+    <ImageLink href="https://laravel.com" icon="mdi:laravel" text="Laravel" />,
+    <ImageLink href="https://symfony.com" icon="mdi:symfony" text="Symfony" />
     and
     <ImageLink href="https://vuejs.org" icon="mdi:vuejs" text="VueJS" />
-    .
+    using
+    <ImageLink href="https://nuxt.com" icon="mdi:nuxt" text="Nuxt" />.
   </p>
 </template>
